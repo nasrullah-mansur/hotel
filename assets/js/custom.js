12cc2a0024b26a9fefe09473afe77386d74d4e1a
nasrullahmansur <nasrullah.cit.bd@gmail.com>
2021-12-07 16:36:17 +0600

    project start

---
+++ assets/js/custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    "use strict";
+
+    
+    $('.banner .slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+    });
+
+    $('.testimonial .slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+
+    $('.show-search').on('click', (e) => {
+        e.preventDefault();
+        $('.search-form').addClass('active');
+    })
+
+    $('.search-form .close').on('click', () => {
+        $('.search-form').removeClass('active');
+    })
+
+    $(window).on('scroll', () => {
+        let scrollTop = $(this).scrollTop();
+
+        if(scrollTop > 200) {
+            $('.header').addClass('fixed-top');
+        } else {
+            $('.header').removeClass('fixed-top');
+        }
+    })
+
+
+    new VenoBox({
+        selector: '.my-video-links',
+    });
+});
